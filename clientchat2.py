691a5e45e3eae2a6efd5275e65c9eb406c83a501
NanangTaufan <nanangtebs@gmail.com>
2015-09-28 22:09:25 -0800

    Create clientchat2.py

---
+++ clientchat2.py
@@ -0,0 +1,22 @@
+#clientchat2.py
+#nanang taufan
+#5113100183
+import sys
+import socket
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 1010)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+
+
+while True:
+    # Send data
+    data = sock.recv(512)
+    print >>sys.stderr, 'client: "%s"' % data
+    message = raw_input("ketik pesan: ")
+    sock.sendall(message)
+    
